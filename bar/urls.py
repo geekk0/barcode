c1a825f473bbebf0b102b5531669d39a87f27c85
gekk0 <gekk0dw@gmail.com>
2023-03-02 15:24:19 +0300

    migrations

--- bar/urls.py
+++ bar/urls.py
@@ -4,7 +4,6 @@ from bar import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path("", views.menu, name=""),
     path("Меню", views.menu, name="Меню"),
